3a1e7afb19fe6d7fd858e73129d58093597dfe2c
kittisak101 <kittiisak_b@cmu.ac.th>
2023-02-17 17:58:58 +0700

    a;sflka;lask;fa

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,19 +20,99 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string f,vector<string> &names,vector<int> &score,vector<char> &grade ){
+
+    ifstream source(f.c_str());
+    string texts;
+    int i=0;
+
+    while(getline(source, texts)){
+
+
+    char format[]= "%[^:]: %d %d %d";
+    char name[50];
+    int a,b,c;
+    sscanf(texts.c_str(),format,name,&a,&b,&c);
+
+    names.push_back(name);
+    score.push_back(a + b + c);
+    grade.push_back(score2grade(score[i]));
+    
+    i++;
+    
+
+
+    }
+
+    
+}
+
+void getCommand(string &command,string &key){
+string h;
+cout << "Please input your command: ";
+getline(cin,h);
+
+int start = 0;
+int end = h.find_first_of(" ");
+
+command = h.substr(start,end-start);
+start = end + 1 ;
+end = h.find_first_of(" ",start);
+
+key =  h.substr(start);
+
+
 
 }
 
-void getCommand(){
+void searchName(vector<string> names,vector<int>  scores,vector<char>  grades,string key){
+
+bool cannotF = false;
+cout << "---------------------------------\n";
+for (unsigned int i = 0; i < names.size() ; i++)
+{
+
+    
+    
+    if(toUpperStr(names[i]) == toUpperStr(key) ){
+    cannotF = true;
+    cout <<names[i]<<"'s score = " << scores[i]<< "\n"; 
+    cout <<names[i]<<"'s grade = " << grades[i]<< "\n"; 
+ 
+    }
+
+}
+if (cannotF == false)
+{
+    cout << "Cannot found.\n";
+}
+
+
+cout << "---------------------------------\n";
+
+
 
 }
 
-void searchName(){
+void searchGrade(vector<string> names,vector<int>  scores,vector<char>  grades,string key){
+
+    cout << "---------------------------------\n";
+
+    for (unsigned int i = 0; i < names.size() ; i++)
+{
+
+    string g;
+    g = grades[i];
+    if(g == key ){
+
+    cout <<names[i]<<" ("<< scores[i]<< ")\n"; 
+ 
+    }
+
+    
 
 }
-
-void searchGrade(){
+    cout << "---------------------------------\n";
 
 }
 
